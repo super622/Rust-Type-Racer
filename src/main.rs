fbf0bd3df5be6ddfb6047f583a51a142e442e5a5
YelloRose <masonrose622@gmail.com>
2022-02-01 19:15:52 +0200

    implement loading words from dictionary

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,10 @@ use type_racer::assets::TextSprite;
 use type_racer::entities::Word;
 use type_racer::debug;
 
+use std::str;
 use std::env;
 use std::path;
+use std::io::Read;
 
 fn main() {
     let conf = Conf::new()
@@ -47,16 +49,28 @@ struct MainState {
     words: Vec<Word>,
     time_until_next_word: f32,
     screen_width: f32,
-    screen_height: f32
+    screen_height: f32,
+    words_pool: Vec<String>
 }
 
 impl MainState {
-    const WORDS: [&'static str; 6] = [
-        "Rust", "programming", "community",
-        "rusty", "random", "facility"
-    ];
+    fn new(ctx: &mut Context, conf: &Conf) -> GameResult<MainState> {
 
-    fn new(_ctx: &mut Context, conf: &Conf) -> GameResult<MainState> {
+        let file = filesystem::open(ctx, "/words.dict");
+        
+        if file.is_err() {
+            panic!("Missing words dictionary!");
+        }
+
+        let mut buffer = Vec::new();
+        let read_size = file?.read_to_end(&mut buffer);
+
+        if read_size.is_err() || read_size? == 0 {
+            panic!("Empty file with words dictionary!");
+        }
+
+        let words = str::from_utf8(&buffer).unwrap().split('\n').collect::<Vec<&str>>();
+        let words = words.iter().map(|x| x.to_string());
 
         let start_state = MainState {
             rng: rand::thread_rng(),
@@ -67,7 +81,8 @@ impl MainState {
             words: Vec::new(),
             time_until_next_word: 4.0,
             screen_width: conf.window_mode.width,
-            screen_height: conf.window_mode.height
+            screen_height: conf.window_mode.height,
+            words_pool: words.collect()
         };
 
         Ok(start_state)
@@ -95,14 +110,14 @@ impl event::EventHandler for MainState {
                     y: self.rng.gen_range(0.0 .. self.screen_height - 100.0)
                 };
             
-                let random_word = Self::WORDS[self.rng.gen_range(0 .. Self::WORDS.len())];
+                let random_word = self.words_pool[self.rng.gen_range(0 .. self.words_pool.len())].clone();
                 let random_speed = self.rng.gen_range(50.0 .. 200.0);
     
-                let word_sprite = Box::new(TextSprite::new(random_word, ctx)?);
-                let word = Word::new(random_word, random_point, random_speed, word_sprite)?;
+                let word_sprite = Box::new(TextSprite::new(&random_word, ctx)?);
+                let word = Word::new(&random_word, random_point, random_speed, word_sprite)?;
     
                 self.words.push(word);
-                self.time_until_next_word = self.rng.gen_range(0.5 .. 1.8);
+                self.time_until_next_word = self.rng.gen_range(2.5 .. 4.8);
             }
 
             for word in self.words.iter_mut() {
@@ -110,7 +125,7 @@ impl event::EventHandler for MainState {
     
                 if word.label() == self.current_input {
                     word.is_typed = true;
-
+                    self.score += 10;
                     // clear the input field after successfully typing word
                     self.current_input = String::new();
                 }
@@ -232,6 +247,8 @@ impl event::EventHandler for MainState {
             return Ok(())
         }
 
+        let label_margin = 10.0;
+
         // Draw current user input
         let font = graphics::Font::new(ctx, "/RedHatDisplay-Regular.otf")?;
         let mut current_input = graphics::Text::new(format!("Input: {}", self.current_input));
@@ -242,6 +259,26 @@ impl event::EventHandler for MainState {
             y: (self.screen_height - current_input.height(ctx))
         };
         graphics::draw(ctx, &current_input, graphics::DrawParam::default().dest(bottom_left))?;
+
+        // Draw current score
+        let mut score_label = graphics::Text::new(format!("Score: {}", self.score));
+        score_label.set_font(font, graphics::PxScale::from(40.0));
+
+        let bottom_right = Point2 {
+            x: (self.screen_width - score_label.width(ctx) - label_margin),
+            y: (self.screen_height - score_label.height(ctx))
+        };
+        graphics::draw(ctx, &score_label, graphics::DrawParam::default().dest(bottom_right))?;
+
+        // Draw remaining lifes
+        let mut lifes_label = graphics::Text::new(format!("Lifes: {}", self.remaining_lifes));
+        lifes_label.set_font(font, graphics::PxScale::from(40.0));
+
+        let next_to_score = Point2 {
+            x: (self.screen_width - score_label.width(ctx) - lifes_label.width(ctx) - label_margin * 2.0),
+            y: (self.screen_height - lifes_label.height(ctx))
+        };
+        graphics::draw(ctx, &lifes_label, graphics::DrawParam::default().dest(next_to_score))?;
 
         for word in self.words.iter_mut() {
             word.draw(ctx)?;
